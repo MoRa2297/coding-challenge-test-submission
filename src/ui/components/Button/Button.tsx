a888b99422cff38d7f82ecc9439067f110aa7541
Manuel Morandin <manuel.morandin@gmail.com>
2026-02-21 16:15:18 +1100

    Updated button component

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,36 +1,37 @@
 import { ButtonType, ButtonVariant } from "@/types";
 import React, { FunctionComponent } from "react";
+import cx from "classnames";
 
 import $ from "./Button.module.css";
 
 interface ButtonProps {
-  onClick?: () => void;
-  type?: ButtonType;
-  variant?: ButtonVariant;
-  loading?: boolean;
-  children: React.ReactNode;
+    onClick?: () => void;
+    type?: ButtonType;
+    variant?: ButtonVariant;
+    loading?: boolean;
+    children: React.ReactNode;
 }
 
 const Button: FunctionComponent<ButtonProps> = ({
-  children,
-  onClick,
-  type = "button",
-  variant = "primary",
-  loading = false,
-}) => {
-  return (
-    <button
-      // TODO: Add conditional classNames
-      // - Must have a condition to set the '.primary' className
-      // - Must have a condition to set the '.secondary' className
-      // - Display loading spinner per demo video. NOTE: add data-testid="loading-spinner" for spinner element (used for grading)
-      className={$.button}
-      type={type}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+    children,
+    onClick,
+    type = "button",
+    variant = "primary",
+    loading = false,
+                                                }) => {
+    return (
+        <button
+            className={cx($.button, {
+                [$.primary]: variant === "primary",
+                [$.secondary]: variant === "secondary",
+            })}
+            type={type}
+            onClick={onClick}
+            disabled={loading}
+        >
+            {loading ? <span data-testid="loading-spinner">⏳</span> : children}
+        </button>
+    );
 };
 
 export default Button;
